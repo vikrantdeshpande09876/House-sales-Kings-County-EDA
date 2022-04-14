8f3bc36bc547baf570457c88341687d0f4bedef5
Vikrant Deshpande <vikrant.deshpande09876@gmail.com>
2022-04-13 22:54:45 -0400

    Initial Commit: started code

---
+++ Codebase/Vikrant_Deshpande_FinalProject.R
@@ -0,0 +1,138 @@
+---
+title: "Final Project"
+author: "Vikrant Deshpande, Tanvi Kolhatkar, Saishree Godbole"
+date: "04/13/2022"
+output: html_document
+---
+  
+  
+  # INTRODUCTION
+  In this project, we look at a dataset of home selling-prices in King County, Washington, between May 2014 and May 2015. The dataset contains 21,613 homes and 21 descriptive features. We will build a multiple regression model with the following variables: y is the outcome variable, and there are two explanatory variables:
+
+1. A numerical explanatory variable x1: sqft living, which refers to the size of a house in square feet of living space. It is worth noting that 1 square foot equals 0.09 square meters.
+
+2. A categorical explanatory variable x2: housing condition, a five-level categorical variable with 1 indicating "poor" and 5 indicating "outstanding."
+
+
+
+require(rio)
+require(tidyverse)
+require(broom)
+require(directlabels)
+require(gridExtra)
+require(skimr)
+require(corrplot)
+
+
+
+
+house_prices <- import('kc_house_data.csv')
+glimpse(house_prices)
+colnames(house_prices)
+
+
+house_prices %>%
+  select(price, sqft_living, condition) %>%
+  skim()
+
+
+house_prices %>%
+  group_by(yr_renovated) %>%
+  summarize(c=n())
+
+
+#INFER RELEVANT FEATURES FROM CORRELATION PLOT: sqft_living
+NUMERIC_COLS <- house_prices %>%
+  lapply(is.numeric) %>%
+  unlist() %>%
+  names() %>%
+  setdiff(c('id', 'date', 'condition', 'yr_built', 'grade', 'zipcode', 'lat', 'long', 'yr_renovated', 'view', 'waterfront'))
+
+house_prices %>%
+  select(NUMERIC_COLS) %>%
+  cor() %>%
+  corrplot(
+    method='color', title='Correlation Matrix for all numerical features', outline=T, addgrid.col='darkgray',
+    order='hclust', addrect=2, rect.lwd=2.5, tl.col='black',
+    addCoef.col="white", number.digits=2, number.cex=0.75
+    )
+
+
+
+# UNIVARIATE PLOTS
+# Histogram of price:
+house_prices %>%
+  ggplot(aes(x=price)) +
+  geom_histogram(color="white") +
+  labs(x="price (USD)", title="House price")
+
+# Histogram of sqft_living:
+ggplot(house_prices, aes(x=sqft_living)) +
+  geom_histogram(color="white") +
+  labs(x="living space (square feet)", title="House size")
+
+
+# Barplot of condition:
+ggplot(house_prices, aes(x=condition)) +
+  geom_bar() +
+  labs(x="condition", title="House condition")
+
+
+
+
+
+
+
+# Univariate plots show right skewed distribution, so performing Box-cox log transformation.
+house_prices <- house_prices %>%
+  mutate(
+    log10_price=log(price),
+    log10_size=log(sqft_living)
+  )
+
+
+
+house_prices %>% 
+  select(price=price) %>%
+  mutate(type='Raw data') %>%
+  union_all(house_prices %>% 
+              select(price=log10_price) %>%
+              mutate(type='After Transformation') 
+        ) %>%
+  ggplot(mapping=aes(x=price, fill=type)) +
+  geom_histogram(alpha=0.5) +
+  facet_wrap(~type, scales='free') +
+  labs(title='Histogram for House-prices', x='Price', y='Count') +
+  theme_bw()
+  
+
+house_prices %>% 
+  select(size=sqft_living) %>%
+  mutate(type='Raw data') %>%
+  union_all(house_prices %>% 
+              select(size=log10_size) %>%
+              mutate(type='After Transformation')
+  ) %>%
+  ggplot(mapping=aes(x=size, fill=type)) +
+  geom_histogram(alpha=0.5) +
+  facet_wrap(~type, scales='free') +
+  labs(title='Histogram for House-sizes', x='Size', y='Count') +
+  theme_bw()
+
+
+
+
+
+
+# MULTIVARIATE PLOTS TO IDENTIFY RELATIONSHIPS
+house_prices %>%
+  mutate(house_condition=as.factor(condition)) %>%
+  ggplot(mapping=aes(x=log10_size, y=log10_price)) +
+  geom_jitter(aes(color=house_condition), size=2, alpha=0.5) +
+  geom_smooth(method='lm', se=FALSE, size=1, alpha=0.5, color='green') +
+  facet_wrap(~house_condition) +
+  labs(title="House prices", y="Transformed price", x="Transformed size") +
+  theme_bw()
+
+
+
