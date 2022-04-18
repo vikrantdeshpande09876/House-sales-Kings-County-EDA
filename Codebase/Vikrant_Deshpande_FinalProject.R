1a66572f768e4a56620d55b58c62b7400ebdaa7c
Vikrant Deshpande <vikrant.deshpande09876@gmail.com>
2022-04-17 23:28:57 -0400

    Updated linear-model interpretations

--- Codebase/Vikrant_Deshpande_FinalProject.R
+++ Codebase/Vikrant_Deshpande_FinalProject.R
@@ -22,7 +22,7 @@ require(broom)
 require(gridExtra)
 require(skimr)
 require(corrplot)
-
+require(ggmap)
 
 
 
@@ -131,8 +131,8 @@ house_prices %>%
   cor() %>%
   corrplot(
     method='color', title='Correlation Matrix for all numerical features', outline=T, addgrid.col='darkgray',
-    order='hclust', addrect=2, rect.lwd=2.5, tl.col='black', tl.cex = 0.75,addtextlabel="ld",
-    addCoef.col="white", number.digits=2, number.cex=0.75
+    order='hclust', addrect=2, rect.lwd=2.5, tl.col='black', tl.cex=0.75
+    addCoef.col='white', number.digits=2, number.cex=0.75
   )
 
 
@@ -151,6 +151,7 @@ house_prices %>%
   facet_wrap(~house_condition) +
   labs(title="House prices faceted by Condition", y="Transformed price", x="Transformed size") +
   theme_bw()
+
 
 
 
@@ -174,6 +175,8 @@ house_prices %>%
   labs(title="House prices faceted by Bedrooms", y="Transformed price", x="Transformed size") +
   theme_bw()
 
+
+
 house_prices %>%
   mutate(house_bathrooms=as.factor(bathrooms)) %>%
   ggplot(mapping=aes(x=log10_size, y=log10_price)) +
@@ -183,46 +186,59 @@ house_prices %>%
   labs(title="House prices faceted by Bathrooms", y="Transformed price", x="Transformed size") +
   theme_bw()
 
-# house_prices %>%
-#   mutate(house_bedrooms=as.factor(bedrooms)) %>%
-#   ggplot(mapping=aes(x=log10_size, y=log10_price)) +
-#   geom_point(alpha = 0.05) +
-#   geom_smooth(method = "lm", se = FALSE,aes(color=house_bedrooms)) +
-#   labs(y = "log10 price",
-#        x = "log10 size",
-#        title = "House prices in Seattle")
 
-my_sf <- house_prices %>%
-  st_as_sf(coords=c('long','lat')) %>%
-  st_set_crs(4326)
+# CHECK IF LOCATION PLAYS A ROLE IN DETERMINING PRICES
+register_google(key='AIzaSyDf9y_5spYHDjQ-jOoDSSXeYIxmQWInBqU', write=T)
+get_googlemap(center='King County, Washington', zoom=9, maptype='hybrid', source='google') %>%
+  ggmap() +
+  geom_point(data=house_prices, aes(x=long, y=lat, color=log10_price), alpha=0.2) +
+  scale_color_gradient('Prices', low='red', high='green') +
+  theme_bw() +
+  labs(
+    title='Map of King County house prices',
+    subtitle='Central, West, and North regions show high priced homes',
+    y='Latitude', x='Longitude'
+    )
 
-my_sf %>%
-  ggplot() +
-  geom_sf(aes(color=log10_price)) +
-  theme_bw() + 
-  theme(panel.background = element_rect(fill = "aliceblue"),
-        axis.ticks = element_blank(),
-        axis.text = element_blank(),
-        panel.grid = element_line(color = "white", size = 0.8)) +
-  coord_sf()
+
 
 
 # CHOOSING THE TOP 5 MOST RELEVANT FEATURES FROM CORRELATION HEATMAP
 house_prices %>%
   select(-c(log10_price,price,date,sqft_living, sqft_living15)) %>%
-  map_dbl(cor, y = house_prices$log10_price) %>%
-  sort(decreasing = TRUE) %>%
+  map_dbl(cor, y=house_prices$log10_price) %>%
+  sort(decreasing=TRUE) %>%
   .[1:6] %>%
   names %>%
   house_prices[.]
 
 
 # CREATING A LINEAR REGRESSIION MODEL AND PERFORMING ANOVA TEST FOR EVAL
-price_interaction <- lm(log10_price ~ (log10_size+grade+bathrooms+bedrooms)^2,
-                        data = house_prices)
+raw_model <- lm(data=house_prices, formula=log10_price ~ (log10_size+grade+bathrooms+bedrooms)^2)
 
 
-summary(price_interaction)
+summary(raw_model)
+
+anova(raw_model)
+
+model_coefs <- tidy(raw_model, conf.int=TRUE)
+model_coefs[-1, ] %>%
+  ggplot(aes(x=estimate, y=term, xmin=conf.low, xmax=conf.high)) +
+  geom_point(size=2) +
+  geom_errorbarh(size=1) +
+  geom_vline(xintercept=0, size=1) +
+  theme_bw() +
+  labs(title='Linear Model coefficient estimates', subtitle='All interactions between predictors considered', x='Estimate', y='Predictor variable')
 
 
-anova(price_interaction)
+model <- lm(data=house_prices, formula=log10_price ~ 
+              (log10_size+grade+bathrooms+bedrooms)+
+              (log10_size*bathrooms + log10_size*bedrooms))
+model_coefs <- tidy(model, conf.int=TRUE)
+model_coefs[-1, ] %>%
+  ggplot(aes(x=estimate, y=term, xmin=conf.low, xmax=conf.high)) +
+  geom_point(size=2) +
+  geom_errorbarh(size=1) +
+  geom_vline(xintercept=0, size=1) +
+  theme_bw() +
+  labs(title='Linear Model coefficient estimates', subtitle='Only important interactions between predictors considered', x='Estimate', y='Predictor variable')
