16a70c5da996959ab268f2fd0478851cdee572f6
Vikrant Deshpande <vikrant.deshpande09876@gmail.com>
2022-04-18 20:53:32 -0400

    Updated subtitles for all plots and added in linear model coefficient plots

--- Codebase/Vikrant_Deshpande_FinalProject.R
+++ Codebase/Vikrant_Deshpande_FinalProject.R
@@ -1,7 +1,7 @@
 ---
-title: "Final Project"
-author: "Vikrant Deshpande, Tanvi Kolhatkar, Saishree Godbole"
-date: "04/13/2022"
+title: 'Final Project'
+author: 'Vikrant Deshpande, Tanvi Kolhatkar, Saishree Godbole'
+date: '04/13/2022'
 output: html_document
 ---
   
@@ -9,7 +9,7 @@ output: html_document
   # INTRODUCTION
   In this project, we look at a dataset of home selling-prices in King County, Washington, between May 2014 and May 2015. The dataset contains 21,613 homes and 21 descriptive features. We will build a multiple regression model with the following variables: y is the outcome variable, and there are two explanatory variables:
 
-    a. Analyze the relationships between features of houses and house-prices, for example- `sqft living`, refers to the square feet of living space, and `housing condition` is a five-level rating with 1 indicating "poor" and 5 indicating "outstanding.".
+    a. Analyze the relationships between features of houses and house-prices, for example- `sqft living`, refers to the square feet of living space, and `housing condition` is a five-level rating with 1 indicating 'poor' and 5 indicating 'outstanding.'.
     b. Check if any other factors affect the house-price apart from the Square Footage, Condition, Bathrooms,and Grade.
     c. Identify if the relationship between features and home-price can be fitted by a linear model and explain any differences.
 
@@ -26,7 +26,7 @@ require(ggmap)
 
 
 
-house_prices <- import('kc_house_data.csv')
+house_prices <- import('C:\\Users\\vikra\\OneDrive\\Desktop\\Vikrant\\GIT_Repos\\House-sales-Kings-County-EDA\\Codebase\\kc_house_data.csv')
 glimpse(house_prices)
 colnames(house_prices)
 
@@ -34,8 +34,7 @@ colnames(house_prices)
 house_prices %>%
   group_by(id) %>%
   summarize(cnt=n(), min_prices=min(price), max_price=max(price)) %>%
-  filter(cnt>1) %>%
-  View()
+  filter(cnt>1)
 
 # REMOVING DIRTY DATA: HOUSES WITH 0 BEDROOMS AND 0 BATHROOMS, 1 HOUSE WITH 33 BEDROOMS BUT SMALL AREA
 house_prices <- house_prices %>%
@@ -43,9 +42,6 @@ house_prices <- house_prices %>%
   filter(bedrooms!=33)
 
 
-house_prices %>%
-  select(price, sqft_living, condition) %>%
-  skim()
 
 
 house_prices %>%
@@ -58,19 +54,19 @@ house_prices %>%
 # Histogram of price:
 house_prices %>%
   ggplot(aes(x=price)) +
-  geom_histogram(color="white") +
-  labs(x="price (USD)", title="House price")
+  geom_histogram(color='white') +
+  labs(x='price (USD)', title='House price')
 
 # Histogram of sqft_living:
 ggplot(house_prices, aes(x=sqft_living)) +
-  geom_histogram(color="white") +
-  labs(x="living space (square feet)", title="House size")
+  geom_histogram(color='white') +
+  labs(x='living space (square feet)', title='House size')
 
 
 # Barplot of condition:
 ggplot(house_prices, aes(x=bedrooms)) +
   geom_bar() +
-  labs(x="condition", title="House condition")
+  labs(x='condition', title='House condition')
 
 
 
@@ -88,10 +84,10 @@ house_prices <- house_prices %>%
 
 
 house_prices %>% 
-  select(price=price) %>%
+  dplyr::select(price=price) %>%
   mutate(type='I. Raw Data') %>%
   union_all(house_prices %>% 
-              select(price=log10_price) %>%
+              dplyr::select(price=log10_price) %>%
               mutate(type='II. Transformed Data') 
         ) %>%
   ggplot(mapping=aes(x=price, fill=type)) +
@@ -102,10 +98,10 @@ house_prices %>%
   
 
 house_prices %>% 
-  select(size=sqft_living) %>%
+  dplyr::select(size=sqft_living) %>%
   mutate(type='I. Raw Data') %>%
   union_all(house_prices %>% 
-              select(size=log10_size) %>%
+              dplyr::select(size=log10_size) %>%
               mutate(type='II. Transformed Data')
   ) %>%
   ggplot(mapping=aes(x=size, fill=type)) +
@@ -116,10 +112,26 @@ house_prices %>%
 
 
 
+# DISTRIBUTION OF BATHROOMS AND BEDROOMS IN OUR ENTIRE DATASET
+house_prices %>%
+  mutate(
+    floor=as.factor(floors), 
+    bedroom=as.factor(bedrooms),
+    bathroom=as.factor(as.integer(bathrooms)),
+    cat_price=ifelse(log10_price<=13, '0', ifelse(log10_price<=14, '1', '2'))
+  ) %>%
+  ggplot() +
+  geom_mosaic(aes(product(bathroom, bedroom), fill=bathroom)) +
+  labs(
+    title='Mosaic Plot for Bedrooms vs Bathrooms',
+    subtitle='Most have 1-3 bedrooms and 1-3 bathrooms',
+    y='Bathrooms', x='Bedrooms'
+    )
 
 
 
-#INFER RELEVANT FEATURES FROM CORRELATION PLOT: sqft_living
+
+# INFER RELEVANT FEATURES FROM CORRELATION PLOT: sqft_living
 NUMERIC_COLS <- house_prices %>%
   lapply(is.numeric) %>%
   unlist() %>%
@@ -127,14 +139,13 @@ NUMERIC_COLS <- house_prices %>%
   setdiff(c('id', 'date', 'yr_built', 'zipcode', 'view', 'yr_renovated', 'waterfront', 'price', 'sqft_living'))
 
 house_prices %>%
-  select(NUMERIC_COLS) %>%
+  dplyr::select(NUMERIC_COLS) %>%
   cor() %>%
   corrplot(
     method='color', title='Correlation Matrix for all numerical features', outline=T, addgrid.col='darkgray',
-    order='hclust', addrect=2, rect.lwd=2.5, tl.col='black', tl.cex=0.75
-    addCoef.col='white', number.digits=2, number.cex=0.75
+    order='hclust', addrect=2, rect.lwd=2.5, tl.col='black', tl.cex=0.75,
+    addCoef.col='white', number.digits=2, number.cex=0.75, mar=c(0,0,1,0)
   )
-
 
 
 
@@ -149,10 +160,29 @@ house_prices %>%
   geom_jitter(aes(color=house_condition), size=2, alpha=0.5) +
   geom_smooth(method='lm', se=FALSE, size=1, alpha=0.5, color='green') +
   facet_wrap(~house_condition) +
-  labs(title="House prices faceted by Condition", y="Transformed price", x="Transformed size") +
+  labs(
+    title='House prices faceted by Condition',
+    subtitle='Not much interaction observed',
+    y='Transformed price', x='Transformed size'
+    ) +
   theme_bw()
 
 
+
+
+
+house_prices %>%
+  mutate(house_bedrooms=as.factor(bedrooms)) %>%
+  ggplot(mapping=aes(x=log10_size, y=log10_price)) +
+  geom_jitter(aes(color=house_bedrooms), size=2, alpha=0.5) +
+  geom_smooth(method='lm', se=FALSE, size=1, alpha=0.5, color='green') +
+  facet_wrap(~house_bedrooms) +
+  labs(
+    title='House prices faceted by Number of Bedrooms',
+    subtitle='Some interesting trends observed',
+    y='Transformed price', x='Transformed size'
+  ) +
+  theme_bw()
 
 
 
@@ -162,18 +192,14 @@ house_prices %>%
   geom_jitter(aes(color=house_grade), size=2, alpha=0.5) +
   geom_smooth(method='lm', se=FALSE, size=1, alpha=0.5, color='green') +
   facet_wrap(~house_grade) +
-  labs(title="House prices faceted by Grade", y="Transformed price", x="Transformed size") +
+  labs(
+    title='House prices faceted by Grade',
+    subtitle='Some interesting trends observed',
+    y='Transformed price', x='Transformed size'
+    ) +
   theme_bw()
 
 
-house_prices %>%
-  mutate(house_bedrooms=as.factor(bedrooms)) %>%
-  ggplot(mapping=aes(x=log10_size, y=log10_price)) +
-  geom_jitter(aes(color=house_bedrooms), size=2, alpha=0.5) +
-  geom_smooth(method='lm', se=FALSE, size=1, alpha=0.5, color='green') +
-  facet_wrap(~house_bedrooms) +
-  labs(title="House prices faceted by Bedrooms", y="Transformed price", x="Transformed size") +
-  theme_bw()
 
 
 
@@ -183,7 +209,11 @@ house_prices %>%
   geom_jitter(aes(color=house_bathrooms), size=2, alpha=0.5) +
   geom_smooth(method='lm', se=FALSE, size=1, alpha=0.5, color='green') +
   facet_wrap(~house_bathrooms) +
-  labs(title="House prices faceted by Bathrooms", y="Transformed price", x="Transformed size") +
+  labs(
+    title='House prices faceted by Number of Bathrooms',
+    subtitle='Some interesting trends observed',
+    y='Transformed price', x='Transformed size'
+  ) +
   theme_bw()
 
 
@@ -205,12 +235,11 @@ get_googlemap(center='King County, Washington', zoom=9, maptype='hybrid', source
 
 # CHOOSING THE TOP 5 MOST RELEVANT FEATURES FROM CORRELATION HEATMAP
 house_prices %>%
-  select(-c(log10_price,price,date,sqft_living, sqft_living15)) %>%
+  dplyr::select(-c(log10_price, price, date, sqft_living, sqft_living15)) %>%
   map_dbl(cor, y=house_prices$log10_price) %>%
   sort(decreasing=TRUE) %>%
   .[1:6] %>%
-  names %>%
-  house_prices[.]
+  names
 
 
 # CREATING A LINEAR REGRESSIION MODEL AND PERFORMING ANOVA TEST FOR EVAL
@@ -219,7 +248,6 @@ raw_model <- lm(data=house_prices, formula=log10_price ~ (log10_size+grade+bathr
 
 summary(raw_model)
 
-anova(raw_model)
 
 model_coefs <- tidy(raw_model, conf.int=TRUE)
 model_coefs[-1, ] %>%
@@ -241,4 +269,22 @@ model_coefs[-1, ] %>%
   geom_errorbarh(size=1) +
   geom_vline(xintercept=0, size=1) +
   theme_bw() +
-  labs(title='Linear Model coefficient estimates', subtitle='Only important interactions between predictors considered', x='Estimate', y='Predictor variable')
+  labs(
+    title='Linear Model coefficient estimates',
+    subtitle='Only important interactions between predictors considered',
+    x='Estimate', y='Predictor variable'
+    )
+
+
+
+augment(model, type.residuals='pearson') %>%
+  ggplot(aes(x=.fitted, y=.resid)) +
+  geom_point(alpha=0.2, color='green') +
+  geom_smooth(method='lm', size=1, alpha=0.6) +
+  labs(
+    title='Residual plot for Linear model',
+    subtitle=paste0(length(model$coefficients)-1,' features'),
+    x='Fitted values', y='Residuals'
+    ) +
+  scale_color_gradient('Prices', low='lightgreen', high='darkgreen') +
+  theme_bw()
